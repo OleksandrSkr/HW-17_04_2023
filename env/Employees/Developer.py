53021e8e1bd1c8aa7047a10c8ad4a75cb87ec99e
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-04-27 17:55:33 +0300

    def firing

--- env/Employees/Developer.py
+++ env/Employees/Developer.py
@@ -1,7 +1,10 @@
-from QA import QA
+from Employees.QA import QA
 
 class Developer (QA):
     def __init__(self, name, age, unemployed : False ):
         super().__init__(name, age, unemployed: False)
-        
+
+    def Firing (self):
+        unemployed == True
+    
     
--- env/Employees/QA.py
+++ env/Employees/QA.py
@@ -4,4 +4,5 @@ class QA (Employee):
     def __init__(self, name, age, unemployed : False ):
         super().__init__(name, age)
         self._unemployed = unemployed
-    
+    def Firing (self):
+        unemployed == True
